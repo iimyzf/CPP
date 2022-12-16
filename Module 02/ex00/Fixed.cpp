67c0802734fe7c8cd43d70fa05f86fab47011c5a
Youssef Agnaou <Agnaou004@gmail.com>
2022-12-16 01:20:18 +0100

    ex00 module 02

---
+++ Module 02/ex00/Fixed.cpp
@@ -0,0 +1,40 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Fixed.cpp                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: yagnaou <yagnaou@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/12/08 17:49:52 by yagnaou           #+#    #+#             */
+/*   Updated: 2022/12/13 13:03:27 by yagnaou          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Fixed.hpp"
+
+int	Fixed::getRawBits() const {
+	std::cout << "getRawBits member function called" << std::endl;
+	return value;
+}
+
+void	Fixed::setRawBits(int const raw) {
+	value = raw;
+}
+
+Fixed::Fixed() : value(0) {
+	std::cout << "Default constructor called" << std::endl;
+}
+
+Fixed::~Fixed() {
+	std::cout << "Destructor called" << std::endl;
+}
+
+Fixed::Fixed(const Fixed& other) : value(other.value) {
+	std::cout << "Copy constructor called" << std::endl;
+}
+
+Fixed& Fixed::operator=(const Fixed& other) {
+	std::cout << "Copy assignment operator called" << std::endl;
+	value = other.value;
+	return *this;
+}
